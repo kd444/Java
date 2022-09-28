d8ffca4b4778b3d1aa69e103942caf94ea55e581
kd444 <karkeradarshan98@gmail.com>
2022-09-28 23:28:11 +0530

    Default Method , java 8 feature

--- interface/DemoInterface/DemoInterface.java
+++ interface/DemoInterface/DemoInterface.java
@@ -1,5 +1,16 @@
+// explanation of default method
+// we cannot make an object of interface
 interface Phone {
     void call();
+
+    // void message(); if we want to add new features but we have provide this to
+    // others
+    // they will need to change their methods
+    // so with declaration add definition
+    // by default all methods in interface are public abstract
+    default void message() {
+        System.out.println("sent message");
+    }
 }
 
 class AndroidPhone implements Phone {
@@ -10,6 +21,9 @@ class AndroidPhone implements Phone {
 
 public class DemoInterface {
     public static void main(String[] args) {
-
+        // type(interface) name = new class
+        Phone p = new AndroidPhone();
+        p.call();
+        p.message();
     }
 }
